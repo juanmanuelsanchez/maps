69d196b415c1a2a7dcbbf8aa9f6bdec4b5a8daa9
Juan Manuel Sánchez <juanmanuel.sanchez@icloud.com>
2015-03-21 23:13:10 +0100

    Change some variables

--- js/main.js
+++ js/main.js
@@ -32,26 +32,26 @@
 
         render: function() {
 
-            var $body= $('body');
+        var $body= $('body');
     		var $wikiElem= $('#wikipedia-links');
     		var $nytHeaderElem= $('#nytimes-header');
     		var $nytElem= $('#nytimes-articles');
     		var $greeting= $('#greeting');
     		    $wikiElem.text("");
-                $nytElem.text("");
+            $nytElem.text("");
 
-    		var streetStr= $('#street').val();
-	        var cityStr= $('#city').val();
-	        var address= streetStr + ',' + cityStr;
-                $greeting.text("So you want to live at " + address + " ? ");
-                console.log(address);
-            var nyTimesUrl= 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +cityStr+ '&sort=newest&api-key=f0331a394e4a5cd0ad8270cfcf7332a7:14:70756973';
+    		this.streetStr= $('#street').val();
+	      this.cityStr= $('#city').val();
+	      this.address= this.streetStr + ',' + this.cityStr;
+                $greeting.text("So you want to live at " + this.address + " ? ");
+                console.log(this.address);
+            var nyTimesUrl= 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +this.cityStr+ '&sort=newest&api-key=f0331a394e4a5cd0ad8270cfcf7332a7:14:70756973';
  
-            var wikipediaUrl='http://en.wikipedia.org/w/api.php?action=opensearch&search=' +cityStr+ '&format=json&callback=wikiCallback';
+            var wikipediaUrl='http://en.wikipedia.org/w/api.php?action=opensearch&search=' +this.cityStr+ '&format=json&callback=wikiCallback';
              
 
             $.getJSON( nyTimesUrl, function( data ) {
-	           $nytElem.text('New York Times articles about: ' + cityStr);
+	           $nytElem.text('New York Times articles about: ' + this.cityStr);
                var articles= data.response.docs;
                for (var i=0; i< articles.length; i++) {
 	
@@ -88,7 +88,7 @@
 		       }
 	       });
 	
-	        console.log(cityStr);
+	        console.log(this.cityStr);
             return false;
 
     	},
