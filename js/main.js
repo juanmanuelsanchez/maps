800301de9445028528b277122127ba22fcc34ab9
Juan Manuel Sánchez <juanmanuel.sanchez@icloud.com>
2015-03-22 15:59:33 +0100

    Add Google places and marker functionality

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@
 
       currentCity:null,
       currentStreet:null,
-      currentAddress:null
+      currentAddress:null,
+      locations:[],
 
   	};
 
@@ -33,6 +34,12 @@
            model.currentAddress= address;
         },
 
+        setCurrentLocations: function(location) {
+
+
+          model.locations.push(location);
+        },
+
         getCurrentCity: function() {
 
           return model.currentCity;
@@ -47,6 +54,11 @@
 
           return model.currentAddress;
         },
+
+        getCurrentLocation: function() {
+
+          return model.locations;
+        }
 
 
   	};
@@ -80,6 +92,7 @@
         octopus.setCurrentStreet(this.streetStr);
         octopus.setCurrentCity(this.cityStr);
         octopus.setCurrentAddress(this.address);
+        octopus.setCurrentLocations(this.address);
 
 
 
@@ -126,7 +139,8 @@
 		       }
 	       });
 
-         view.renderMap();
+         //view.renderMap();
+           view.renderGeocode();
 
 	        console.log(this.cityStr);
             return false;
@@ -169,6 +183,99 @@
         });
 
 
+      },
+
+      renderGeocode: function() {
+
+          var map;
+          var locations;        
+          var mapOptions = {
+          disableDefaultUI: true
+
+          };
+   
+          map = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
+
+
+         function locationFinder() {
+           var i=0;
+           var locationsData= octopus.getCurrentLocation();
+           var locations = [];
+           
+           for(i; i<locationsData.length; i++) {
+            
+               var location= locationsData[i];
+               locations.push(location);
+         }
+      
+
+          console.log(locations);
+          return locations;
+
+        }
+
+
+        function createMapMarker(placeData) {
+
+   
+          var lat = placeData.geometry.location.lat();  
+          var lon = placeData.geometry.location.lng();  
+          var name = placeData.formatted_address;   
+          var bounds = window.mapBounds;            
+
+          var marker = new google.maps.Marker({
+           map: map,
+           position: placeData.geometry.location,
+           title: name
+         });
+    
+         var infoWindow = new google.maps.InfoWindow({
+         content: name
+         });
+
+         google.maps.event.addListener(marker, 'click', function() {
+         infoWindow.open(map, marker);
+         });
+
+         bounds.extend(new google.maps.LatLng(lat, lon));
+    
+         map.fitBounds(bounds);
+
+         map.setCenter(bounds.getCenter());
+
+        }
+
+
+      function callback(results, status) {
+       if (status == google.maps.places.PlacesServiceStatus.OK) {
+       createMapMarker(results[0])
+        }
+      }
+
+      function pinPoster(locations) {
+
+        var service = new google.maps.places.PlacesService(map);
+    
+        for (place in locations) {
+
+        var request = {
+          query: locations[place]
+        }
+
+         service.textSearch(request, callback);
+        }
+      }
+
+        window.mapBounds = new google.maps.LatLngBounds();
+
+  
+        locations = locationFinder();
+        pinPoster(locations);
+
+        window.addEventListener('resize', function(e) {
+        map.fitBounds(mapBounds);
+      });
+      
       },
 
 
